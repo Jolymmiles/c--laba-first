ca9ab94161f3977c84ba74a0d2285aff1207bcf1
Valentine <valmak29@gmail.com>
2022-09-12 16:15:58 +0400

    add new task

--- Program.cs
+++ Program.cs
@@ -10,6 +10,15 @@ class Program
         secondTask();
         Console.WriteLine("---------------------------------\n Task 3");
         thirdTask();
+        Console.WriteLine("---------------------------------\n Task 4");
+        fourthTask();
+        Console.WriteLine("---------------------------------\n Task 5");
+        fifthTask();
+        Console.WriteLine("---------------------------------\n Task 6");
+        sixthTask();
+        Console.WriteLine((int) 'a');
+        Console.WriteLine((int) 'z');
+        printArray(createListOfString(5).ToArray());
     }
 
     static void firstTask()
@@ -56,16 +65,72 @@ class Program
         printArray(array.ToArray());
         Console.WriteLine();
         string l = "2";
-        string last = array.Last(n => (n[0] == '-' ? (n.Length == Convert.ToInt32(l) + 1) : (n.Length == Convert.ToInt32(l))) & (n[0] == '-' ? n[1] == l.ToCharArray()[0] : n[0] == l.ToCharArray()[0]));
-        if (last != null)
+        try
         {
+            string last = array.Last(n => (n[0] == '-' ? (n.Length == Convert.ToInt32(l) + 1) : (n.Length == Convert.ToInt32(l))) & (n[0] == '-' ? n[1] == l.ToCharArray()[0] : n[0] == l.ToCharArray()[0]));
             Console.WriteLine(last);
         }
-        else
+        catch (Exception)
         {
             Console.WriteLine(0);
         }
+    }
 
+    static void fourthTask()
+    {
+        var array = createListWithRandomDigit(20).Select(n => Convert.ToString(n));
+        var symbolC = "5";
+        printArray(array.ToArray());
+        try
+        {
+            var lastElem = array.Single(elem => elem[elem.Length - 1].Equals(symbolC));
+            Console.WriteLine(lastElem);
+        }
+        catch (ArgumentNullException)
+        {
+            Console.WriteLine("\nAnswer: empty");
+        }
+        catch (InvalidOperationException)
+        {
+            Console.WriteLine("\nAnswer: Error");
+        }
+    }
+    /* Даны символ С и строковая последовательность A. Найти количество элементов
+    A, которые содержат более одного символа и при этом начинаются и оканчиваются
+    символом C. */
+    static void fifthTask()
+    {
+        var array = createListWithRandomDigit(20).Select(n => Convert.ToString(n)).ToList();
+        var symbolC = '5';
+        array.Add("55");
+        printArray(array.ToArray());
+
+        try
+        {
+            var countOfElem = array.Count(n => n.Count(e => e.Equals(symbolC)) > 1 & n[0].Equals(symbolC) & n[n.Length - 1].Equals(symbolC));
+            Console.WriteLine("\n" + countOfElem);
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("zero");
+        }
+    }
+    /* Дана целочисленная последовательность. Найти количество ее
+    положительных двузначных элементов, а также их среднее арифметическое (как
+    вещественное число). Если требуемые элементы отсутствуют, то дважды вывести 0
+    (первый раз как целое, второй – как вещественное).*/
+    static void sixthTask()
+    {
+        var array = createListWithRandomDigit(20);
+        printArray(array.ToArray());
+        var countPositive = array.Count(n => n > -1);
+        int sumOfPositiveDigit = array.Where(n => n > 0).Sum();
+        decimal middle = Convert.ToDecimal(sumOfPositiveDigit) / Convert.ToDecimal(countPositive);
+        Console.WriteLine("\nCount: " + countPositive + "\nSum: " + sumOfPositiveDigit + "\nMiddle: " + middle);
+    }
+
+    static void seventhTask()
+    {
 
     }
 
@@ -80,10 +145,25 @@ class Program
     static List<int> createListWithRandomDigit(int lengthOfList)
     {
         Random random = new Random();
-        var array = new List<Int32>();
+        var array = new List<int>();
         for (int i = 0; i < lengthOfList; i++)
         {
             array.Add(random.Next(-100, 100));
+        }
+        return array;
+    }
+
+    static List<string> createListOfString(int lengthOfList)
+    {
+        Random random = new Random();
+        var array = new List<string>();
+        for (int i = 0; i < lengthOfList; i++)
+        {
+            string s = "";
+            for (int j = 0; j < random.Next(3, 10); j++){
+                s+= (char) random.Next(97, 122);
+            }
+            array.Add(s);
         }
         return array;
     }
